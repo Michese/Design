78dff7da7932b4e14b34a7dea8aea4faba41713f
Michese <michese48@mail.ru>
2021-03-10 20:18:52 +0300

    formated

--- second.php
+++ second.php
@@ -4,26 +4,26 @@ echo "<h2>Task 2</h2>";
 $stack = new SplStack();
 $brackets = ['{' => '}', '[' => ']', '(' => ')'];
 $str_all = [
-"\"Это тестовый вариант проверки (задачи со скобками). И вот еще скобки: {[][()]}\"",
-"((a + b)/ c) - 2",
-"\"([ошибка)\"",
-"\"(\")"
+    "\"Это тестовый вариант проверки (задачи со скобками). И вот еще скобки: {[][()]}\"",
+    "((a + b)/ c) - 2",
+    "\"([ошибка)\"",
+    "\"(\")"
 ];
 foreach ($str_all as $key => $str) {
-echo "Строчка номер $key <br>";
-echo $str;
-preg_match_all("/[\"\[\]{})(]/", $str, $strArray);
-foreach ($strArray[0] as $symbol) {
-if ($stack->isEmpty() || $stack->bottom() !== '"' && $brackets[$stack->bottom()] !== $symbol) {
-$stack->push($symbol);
-} else if ($stack->bottom() === '"' && $symbol === '"' || $brackets[$stack->bottom()] === $symbol) {
-$stack->pop();
-}
-}
-echo " - ";
-var_dump($stack->isEmpty());
-while (!$stack->isEmpty()) {
-$stack->pop();
-}
-echo "<br>";
+    echo "Строчка номер $key <br>";
+    echo $str;
+    preg_match_all("/[\"\[\]{})(]/", $str, $strArray);
+    foreach ($strArray[0] as $symbol) {
+        if ($stack->isEmpty() || $stack->bottom() !== '"' && $brackets[$stack->bottom()] !== $symbol) {
+            $stack->push($symbol);
+        } else if ($stack->bottom() === '"' && $symbol === '"' || $brackets[$stack->bottom()] === $symbol) {
+            $stack->pop();
+        }
+    }
+    echo " - ";
+    var_dump($stack->isEmpty());
+    while (!$stack->isEmpty()) {
+        $stack->pop();
+    }
+    echo "<br>";
 }
